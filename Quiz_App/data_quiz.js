d1cb2ee7e5454e9984c67e049dbb5cb83bb39e77
Elliot <emuculo25@gmail.com>
2022-07-28 20:52:43 +0200

    finsih the quiz app

---
+++ Quiz_App/data_quiz.js
@@ -0,0 +1,45 @@
+
+
+// export const data = [
+//     {
+//         question: 'How old is florin pop',
+//         a: '10',
+//         b: '16',
+//         c: '26',
+//         d: '100',
+//         correct: 'c'
+//     },
+//     {
+//         question: 'The most used programming language in 2019',
+//         a: 'java',
+//         b: 'python',
+//         c: 'C',
+//         d: 'Dart',
+//         correct: 'a'
+//     },
+//     {
+//         question: 'The presidente of mozambique',
+//         a: 'Josh',
+//         b: 'Joe Biden',
+//         c: 'Marcelo Rebelo',
+//         d: 'Filipe Nyusi',
+//         correct: 'd'
+//     },
+//     {
+//         question: 'What does HTML stand for',
+//         a: 'Hypertext markup language',
+//         b: 'Hyperlink make language',
+//         c: 'Http markup language',
+//         d: 'Http markdown language',
+//         correct: 'a'
+//     },
+//     {
+//         question: 'what language was JS launched',
+//         a: '2000',
+//         b: '1987',
+//         c: '2009',
+//         d: 'none of above',
+//         correct: 'd'
+//     },
+// ];
+
---
+++ Quiz_App/index.js
@@ -0,0 +1,140 @@
+
+
+const data = [
+    {
+        question: 'How old is florin pop',
+        a: '10',
+        b: '16',
+        c: '26',
+        d: '67',
+        correct: 'c'
+    },
+    {
+        question: 'The most used programming language in 2019',
+        a: 'java',
+        b: 'python',
+        c: 'C',
+        d: 'Dart',
+        correct: 'a'
+    },
+    {
+        question: 'The presidente of mozambique',
+        a: 'Josh',
+        b: 'Joe Biden',
+        c: 'Marcelo Rebelo',
+        d: 'Filipe Nyusi',
+        correct: 'd'
+    },
+    {
+        question: 'What does HTML stand for',
+        a: 'Hypertext markup language',
+        b: 'Hyperlink make language',
+        c: 'Http markup language',
+        d: 'Http markdown language',
+        correct: 'a'
+    },
+    {
+        question: 'what language was JS launched',
+        a: '2000',
+        b: '1987',
+        c: '2009',
+        d: 'none of above',
+        correct: 'd'
+    },
+];
+
+
+const answers = document.querySelectorAll(".answer");
+let selectAnswer = undefined;
+const questionEl = document.getElementById('question');
+const submitEl = document.getElementById('submit');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+let score = 0;
+let current_question = 0;
+
+loadQuiz();
+
+
+/**
+ * ? load the question on radio
+ */
+function loadQuiz() {
+
+    deselect();
+    questionEl.innerHTML = data[current_question].question;
+    a_text.innerHTML = data[current_question].a;
+    b_text.innerHTML = data[current_question].b;
+    c_text.innerHTML = data[current_question].c;
+    d_text.innerHTML = data[current_question].d;
+}
+
+
+/**
+ * ? unselect the radio when data load of form
+ */
+function deselect() {
+    answers.forEach((answer) => {
+        if (answer.checked) {
+            answer.checked = false;
+        }
+    });
+}
+
+/**
+ * ? verify that in all radio there's one that's checked
+ * 
+ * @returns isChecked boolean
+ */
+
+function getSelectedRadio() {
+    answers.forEach((answer) => {
+        if (answer.checked) {
+            selectAnswer = answer.id;
+        }
+    });
+
+    return selectAnswer;
+}
+
+/**
+ * ? when click the button disparar evento click and do the magic
+ */
+
+submitEl.addEventListener('click', () => {
+    const answerValue = getSelectedRadio();
+
+    if (answerValue !== undefined) { 
+        if (answerValue === data[current_question].correct) {
+            ++score;
+        }
+        
+        current_question++;
+        if (current_question < data.length) { 
+            loadQuiz();
+        } else {
+            quiz.innerHTML = score;
+        }
+    } 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
